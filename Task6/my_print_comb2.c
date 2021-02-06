5598f606210b2c0153d98d748baab5bfd21574ae
Lazzarotto Maxime <lazzarottomaxime@macbook-pro-de-maxime-2.home>
2021-02-06 10:41:15 +0100

    task6

--- Task6/my_print_comb2.c
+++ Task6/my_print_comb2.c
@@ -1,33 +1,43 @@
 int my_print_comb2(void)
 {
-  int  a, b, c, d;
-  a = 48;
-   while (a <= 57)
+  char  a = 48, b = 48, c = 48, d = 48; 
+  while (a <= 57)
     {
-      b = a + 1;
-     while (b <= 57)
-       {
-	 c = b + a;
-	 while (c <= 57)
-	  {
-	    d = c + b;
-	   while (d <= 57)
-	     {
-	       my_putchar(a);
-	       my_putchar(b);
-	       my_putchar(' ');
-	       my_putchar(c);
-	       my_putchar(d);
-	       my_putchar(',');
-	       my_putchar(' ');
-	    d++;
-	  }
-	 c++;
-       }
-     b++; 
+      while (b <= 57)
+	{
+	  while (c <= 57)
+	    {
+	      while (d <= 57)
+		{
+		  if (d > b && c >= a)
+		    {
+		      my_putchar(a);
+                      my_putchar(b);
+                      my_putchar(' ');
+                      my_putchar(c);
+                      my_putchar(d);
+		      if (a == 57 && b == 56 && c == 57 && d == 57)
+			{
+			  my_putchar();
+			}
+		      else
+			{
+			  my_putchar(',');
+			  my_putchar(' ');
+			}
+		    }
+		  d++;
+		}
+	      d = 48;
+	      c++;
+	    }
+	  c = 48;
+	  b++; 
+	}
+      b = 48;
+      a++;
     }
-   a++;
+  my_putchar('\n');
+  return (0);
 }
-   my_putchar('\n');
-    return (0);
-}
+
